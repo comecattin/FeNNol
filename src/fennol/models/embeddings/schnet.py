e1f1d633e992ae63985085f5e6f8a55fab1a2a65
Côme Cattin <come.cattin@ens-lyon.fr>
2024-03-26 17:00:48 +0100

    Schnet Embedding file.

---
+++ src/fennol/models/embeddings/schnet.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Implementation of SchNet embedding.
+
+Done by Côme Cattin, 2024.
+"""
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+
+
+class SchNetEmbedding(nn.Module):
+    """SchNet embedding.
+
+    Continuous filter convolutional neural network for modeling quantum
+    interactions.
+
+    References
+    ----------
+    SCHÜTT, Kristof, KINDERMANS, Pieter-Jan, SAUCEDA FELIX, Huziel Enoc, et al.
+    Schnet: A continuous-filter convolutional neural network for
+    modeling quantum interactions.
+    Advances in neural information processing systems, 2017, vol. 30.
+    https://proceedings.neurips.cc/paper_files/paper/2017/file/303ed4c69846ab36c2904d3ba8573050-Paper.pdf
+    """
+
+
+if __name__ == "__main__":
+    pass
