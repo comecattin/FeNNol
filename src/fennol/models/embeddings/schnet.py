0d396208f04fa603a510c992a97c68d25d251cf2
Côme Cattin <come.cattin@ens-lyon.fr>
2024-03-28 11:34:09 +0100

    SSP activation function and clean up

--- src/fennol/models/embeddings/schnet.py
+++ src/fennol/models/embeddings/schnet.py
@@ -3,15 +3,16 @@
 
 Done by Côme Cattin, 2024.
 """
+
 import dataclasses
-from typing import Callable, Dict, Union
+from typing import Callable, Dict, Sequence, Union
 
 import flax.linen as nn
 import jax
-import jax.numpy as jnp
 
-from fennol.models.misc.encodings import RadialBasis, SpeciesEncoding
-from fennol.models.misc.nets import FullyConnectedNet
+from ...utils.activations import ssp
+from ..misc.encodings import RadialBasis, SpeciesEncoding
+from ..misc.nets import FullyConnectedNet
 
 
 class SchNetEmbedding(nn.Module):
@@ -42,18 +43,21 @@ class SchNetEmbedding(nn.Module):
         The radial basis function parameters.
     species_encoding : dict, default={}
         The species encoding parameters.
-    activation : Union[Callable, str], default=nn.softplus
+    activation : Union[Callable, str], default=ssp
         The activation function.
     """
 
     _graphs_properties: Dict
     dim: int = 64
     nlayers: int = 3
+    conv_hidden: Sequence[int] = dataclasses.field(
+        default_factory=lambda: [64, 64]
+    )
     graph_key: str = "graph"
     embedding_key: str = "embedding"
     radial_basis: dict = dataclasses.field(default_factory=dict)
     species_encoding: dict = dataclasses.field(default_factory=dict)
-    activation: Union[Callable, str] = nn.softplus
+    activation: Union[Callable, str] = ssp
 
     FID: str = "SCHNET"
 
@@ -62,14 +66,17 @@ class SchNetEmbedding(nn.Module):
         """Forward pass."""
         species = inputs["species"]
         graph = inputs[self.graph_key]
+        switch = graph["switch"][:, None]
         edge_src, edge_dst = graph["edge_src"], graph["edge_dst"]
         cutoff = self._graphs_properties[self.graph_key]["cutoff"]
         onehot = SpeciesEncoding(
-            **self.species_encoding,
-            name="SpeciesEncoding"
+            **self.species_encoding, name="SpeciesEncoding"
         )(species)
 
-        xi_prev_layer = onehot
+        xi_prev_layer = nn.Dense(
+            self.dim, name="species_linear", use_bias=False
+        )(onehot)
+
         distances = graph["distances"]
         radial_basis = RadialBasis(
             **{
@@ -79,43 +86,39 @@ class SchNetEmbedding(nn.Module):
             }
         )(distances)
 
-        def atom_wise(xi, i):
+        def atom_wise(xi, i, layer):
             return nn.Dense(
-                self.dim,
-                name=f"atom_wise_{i}_{layer}",
-                use_bias=True
+                self.dim, name=f"atom_wise_{i}_{layer}", use_bias=True
             )(xi)
 
         # Interaction layer
         for layer in range(self.nlayers):
-
             # Atom-wise
-            xi = atom_wise(xi_prev_layer, 1)
+            xi = atom_wise(xi_prev_layer, 1, layer)
 
             # cfconv
             w_l = FullyConnectedNet(
-                [self.dim, self.dim],
+                [*self.conv_hidden, self.dim],
                 activation=self.activation,
                 name=f"filter_weight_{layer}",
                 use_bias=True,
             )(radial_basis)
             xi_j = xi[edge_dst]
             xi = jax.ops.segment_sum(
-                w_l * xi_j, edge_src, species.shape[0]
+                self.activation(w_l) * xi_j * switch, edge_src, species.shape[0]
             )
 
             # Atom-wise
-            xi = atom_wise(xi, 2)
+            xi = atom_wise(xi, 2, layer)
 
             # Activation
             xi = self.activation(xi)
 
             # Atom-wise
-            xi = atom_wise(xi, 3)
+            xi = atom_wise(xi, 3, layer)
 
             # Residual connection
-            if layer > 0:
-                xi = xi + xi_prev_layer
+            xi = xi + xi_prev_layer
             xi_prev_layer = xi
 
         output = {
@@ -126,48 +129,4 @@ class SchNetEmbedding(nn.Module):
 
 
 if __name__ == "__main__":
-
-    from jax.random import PRNGKey
-
-    from fennol import FENNIX
-
-    # Test
-    model_water = FENNIX(
-    cutoff=5.0,
-    rng_key=PRNGKey(0),
-    modules={
-            'embedding': {
-                'module_name': 'SCHNET',
-                'dim': 64,
-                'nlayers': 3,
-                'graph_key': 'graph',
-                'embedding_key': 'embedding',
-            },
-        }
-    )
-    coordinates_water = jnp.array(
-        [
-            [
-                [0.0, 0.0, 0.0],
-                [1.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0]
-            ]
-        ]
-    ).reshape(-1, 3)
-
-    species = jnp.array(
-        [
-            [8, 1, 1]
-        ]
-    ).reshape(-1)
-
-    natoms = jnp.array([3])
-    batch_index = jnp.array([0, 0, 0])
-
-    output_water = model_water(
-        species=species,
-        coordinates=coordinates_water,
-        natoms=natoms,
-        batch_index=batch_index
-    )
-
+    pass
